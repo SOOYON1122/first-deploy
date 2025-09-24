fc8c77296e619d6dd5c55d129878b878edcd9f98
sooyon <pksuyon1122@naver.com>
2025-09-24 16:15:45 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -14,14 +14,10 @@ export default function Home() {
         />
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
+            Hi, I'm SOOYON!
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+            Nice to meet you!
           </li>
         </ol>
 
