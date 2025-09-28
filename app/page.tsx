4373b4d4331a806f2fa0f40a59b4bc9b0bed0827
Park Sooyon <pksuyon1122@naver.com>
2025-09-28 23:33:56 +0900

    about 수정

--- app/page.tsx
+++ app/page.tsx
@@ -30,54 +30,26 @@ export default function Home() {
           Docs
         </a>
       </nav>
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        {/* <Image
-          className="dark:invert"
-          src="/prof.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        /> */}
-        {/* <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div> */}
 
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         {/* About 섹션 */}
-        <Image
-          className="dark:invert"
-          src="/prof.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <section id="about" className="w-full flex flex-col items-center mb-8">
-          <h2 className="text-xl font-bold mb-2">About</h2>
-          <p>박 수 연 | PARK SOOYON</p>
-        </section>
+        <div className="flex flex-row items-center gap-6">
+          <Image
+            className="dark:invert"
+            src="/prof.svg"
+            alt="Next.js logo"
+            width={180}
+            height={38}
+            priority
+          />
+          <section
+            id="about"
+            className="w-full flex flex-col items-center mb-8"
+          >
+            <h2 className="text-xl font-bold mb-2">About</h2>
+            <p>박 수 연 | PARK SOOYON</p>
+          </section>
+        </div>
 
         {/* Greeting 섹션 */}
         <section
